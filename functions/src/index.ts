c819a0325cf6c57c82782f1f8a76d57a49f3f570
hsbyte <36382588+hsbyte@users.noreply.github.com>
2020-02-03 01:13:31 -0600

    update index.ts, update dependencies

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,27 @@
+enum HTTP {
+    // Successful responses
+    OK = 200,
+    Created = 201,
+    // Client error responses
+    Unauthorized = 401,
+    // Server error responses
+    InternalServerError = 500,
+}
+
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as express from 'express'
+import * as cors from 'cors'
 import * as dotenv from "dotenv"
 
 dotenv.config()
 const app = express()
-// const cors = require('cors')
+
+app.use(cors(
+    {
+        credentials: true,
+    }
+))
 
 // admin.initializeApp()
 // const db = admin.firestore(functions.config().firebase)
@@ -17,44 +33,44 @@ const db = admin.firestore()
 
 const VERS = process.env.API_VERSION
 
-app.get(`/${VERS}/destinations`, (req: any, res: any) => {
-    db.collection('destinations').get()
+app.get(`/${VERS}/destinations`, (req, res) => {
+    db.collection('destinations')
+        .get()
         .then(snapshot => {
-            const promises: any = []
+            const promises: Array<any> = []
             snapshot.forEach(doc => promises.push(doc.data()))
-            res.json(promises)
+            res.status(HTTP.OK).json(promises)
         })
-        .catch(error => res.status(500).send(error))
+        .catch(error => res.status(HTTP.InternalServerError).send(error))
 })
 
+// Sign up
+app.post(`/signup`, (req, res) => {
+    admin.auth()
+        .createUser({ ...req.body })
+        .then(userRecord => res.status(HTTP.Created).json({
+            status: 'success',
+            ...userRecord
+        }))
+        .catch(error => res.status(HTTP.Unauthorized).send(error))
+})
+
+// Sign in
+app.get('/signin/:id', (req, res) => {
+    console.log(req.params.id)
+
+})
 
 // https://baseurl.com/api/
 exports.api = functions.https.onRequest(app)
 
 
-
-
-
-
-
-
-
 // export const onDestinationsUpdate = functions.firestore.document("destinations").onUpdate(change => {
 //     const after = change.after.data()
 //     const payload: any = {
-//         destinations: after
+//         ...after
 //     }
 //     return admin.messaging().sendToTopic("destinations", payload)
-// })
-
-// export const destinations = functions.https.onRequest((req, res) => {
-//     db.collection('destinations').get()
-//     .then(snapshot => {
-//         const promises: any = []
-//         snapshot.forEach(doc => promises.push(doc.data()))
-//         res.send(promises)
-//     })
-//     .catch(error => res.status(500).send(error))
 // })
 
 
